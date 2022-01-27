bad7bcce6e2a4383083ef879ca57a703df0aa3a2
Vijendra-Achar <vijendra.1995@gmail.com>
2022-01-27 23:34:37 +0530

    The Expense component is now dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
-import ExpenseItem from "./components/Modules/ExpenseItem";
+import ExpenseItem from "./components/Modules/ExpenseItem/ExpenseItem";
 
 function App() {
+  const expenseItems = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      desc: "Example Description One",
+      date: new Date(2020, 7, 14),
+      type: "income",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      desc: "Example Description Two",
+      type: "expense",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      desc: "Example Description Three",
+      date: new Date(2021, 2, 28),
+      type: "income",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      desc: "Example Description Four",
+      date: new Date(2021, 5, 12),
+      type: "expense",
+    },
+  ];
+
   return (
     <div>
-      <ExpenseItem />
-      <ExpenseItem />
+      {expenseItems.map((expense) => {
+        return <ExpenseItem key={expense.id} expense={expense} />;
+      })}
     </div>
   );
 }
---
+++ src/components/Modules/ExpenseItem/ExpenseItem.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import ExpenseDate from "../ExpenseDate/ExpenseDate";
+
+import "./ExpenseItem.scss";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div className="expense-item__date">
+        {props.expense.date.toISOString()}
+      </div>
+      <div className="expense-item__details">
+        <div className="title">{props.expense.title}</div>
+        <div className="description">
+          <p>{props.expense.desc}</p>
+        </div>
+      </div>
+
+      <div className="expense-item__amount expense">
+        $ {props.expense.amount}
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
