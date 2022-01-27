142454b2e5f32838204f1543af412dc3060586f3
Vijendra-Achar <vijendra.1995@gmail.com>
2022-01-27 22:46:27 +0530

    The expense item component is complete ✔️

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/Modules/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <ExpenseItem />
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
